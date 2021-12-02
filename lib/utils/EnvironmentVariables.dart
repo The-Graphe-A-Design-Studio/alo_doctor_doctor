afaa9dbe1af9c24135341cb448e95d18f132e403
Aniket Vishal <aniket.vishal76@gmail.com>
2021-12-02 21:33:48 +0530

    Environment Variable updated

--- lib/utils/EnvironmentVariables.dart
+++ lib/utils/EnvironmentVariables.dart
@@ -1,19 +1,11 @@
 // ***** Development Environment Variables *****
 
-// ** Backend Variables **
-
 // const String authority = 'developers.thegraphe.com';
 // const String commonUnencodedPath = '/alodoctor/public/api';
-
-// // base url
 // const baseUrl = "https://developers.thegraphe.com/alodoctor/public";
 
 // ***** Production Environment Variables *****
 
-// ** Backend Variables **
-
 const String authority = 'www.alodoctor-care.com';
 const String commonUnencodedPath = '/app-backend/public/api';
-
-//base url
 const baseUrl = "https://www.alodoctor-care.com/app-backend/public";
